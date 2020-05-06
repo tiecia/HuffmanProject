7e97f43a4d7b118d956649bae17db21f789281d8
Tie C <usbtyping@gmail.com>
2020-05-05 20:16:38 -0700

    Finish w/ Comments

--- src/HuffmanNode.java
+++ src/HuffmanNode.java
@@ -1,63 +1,132 @@
+/**
+ * @author Tyler Ciapala-Hazlerig
+ * CSE 143 Data Structures
+ * Huffman Coding Project: HuffmanNode
+ */
+
 public class HuffmanNode implements Comparable<HuffmanNode> {
+    /**
+     * Stores the character this node represents.
+     * Is 0 when node does not store a character.
+     */
     private char character;
+
+    /**
+     * Stores the frequency of the given character in the file the overall {@link HuffmanTree} represents.
+     * Stores -1 when frequency is not given, such a constructing from a .code file.
+     */
     private int frequency;
 
+    /**
+     * Stores the left child node
+     */
     private HuffmanNode left;
+
+    /**
+     * Stores the right child node
+     */
     private HuffmanNode right;
 
+    /**
+     * Creates a HuffmanNode with specific character and frequency values.
+     * Used when creating a {@link HuffmanTree} from a decompressed file.
+     *
+     * @param character The character to assign the new node.
+     * @param frequency The frequency to assign the character in this node.
+     */
     public HuffmanNode(char character, int frequency) {
         this.character = character;
         this.frequency = frequency;
     }
 
+    /**
+     * Creates a HuffmanNode with a specific frequency, left node, and right node.
+     * Used when creating a {@link HuffmanTree} from a decompressed file.
+     *
+     * @param frequency The frequency to assign this node.
+     * @param left The HuffmanNode to assign as the left child.
+     * @param right The HuffmanNode to assign as the right child.
+     */
     public HuffmanNode(int frequency, HuffmanNode left, HuffmanNode right) {
         this((char) 0, frequency);
         this.left = left;
         this.right = right;
     }
 
+    /**
+     * Creates a blank "intermediary" HuffmanNode.
+     * Used as intermediary nodes when creating a tree from a .code file.
+     * Frequency is automatically set to -1
+     */
     public HuffmanNode(){
         this.frequency = -1;
     }
 
+    /**
+     * Tests to see if this node is a leaf node.
+     * A leaf node is defined and both children equalling null.
+     *
+     * @return true is node is leaf; false if node is not a leaf.
+     */
     public boolean isLeaf(){
         return this.right == null && this.left == null;
     }
 
+    /**
+     * Used to retrieve the character this node stores.
+     * Note: Characters are automatically set to 0 when not assigned. All "intermediary" nodes will have a
+     * character of zero. To properly test if a node stores a character use getCharacter() and getFrequency() combined.
+     *
+     * @return The character this node stores.
+     */
     public char getCharacter() {
         return character;
     }
 
+    /**
+     * Used to retrieve the frequency the character in the node shows up in the file.
+     *
+     * @return the frequency of the stored character; -1 if the node is an "intermediary" node within the tree.
+     */
     public int getFrequency() {
         return frequency;
     }
 
+    /**
+     * @return the node attached to the left side of this node.
+     */
     public HuffmanNode getLeft() {
         return left;
     }
 
+    /**
+     * @return the node attached to the right of this node.
+     */
     public HuffmanNode getRight() {
         return right;
     }
 
+    /**
+     * @param left the node to assign as the new left child node.
+     */
     public void setLeft(HuffmanNode left) {
         this.left = left;
     }
 
+    /**
+     * @param right the node to assign as the new right child node.
+     */
     public void setRight(HuffmanNode right) {
         this.right = right;
     }
 
+    /**
+     * This method compares this HuffmanNode to another HuffmanNode. Used in the priority queue for sorting.
+     * @param o The node to compare this node to.
+     * @return this node's frequency minus the other node's frequency
+     */
     @Override
     public int compareTo(HuffmanNode o) {
         return this.getFrequency() - o.getFrequency();
-    }
-
-    @Override
-    public String toString() {
-        return "HuffmanNode{" +
-                "character=" + character +
-                ", frequency=" + frequency +
-                '}';
     }
 }
--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,3 +1,9 @@
+/**
+ * @author Tyler Ciapala-Hazlerig
+ * CSE 143 Data Structures
+ * Huffman Coding Project: HuffmanTree
+ */
+
 import java.io.*;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -5,10 +11,22 @@ import java.util.Scanner;
 
 public class HuffmanTree {
 
+    /**
+     * The root of the huffman tree. Will be populated with all nodes once the HuffmanTree(int[] count)
+     * constructor is called
+     */
     HuffmanNode root;
 
+    /**
+     * Creates a Huffman tree based on th frequency's given in the passed in array.
+     * If a number in the array is greater than 0 then a node is created with the character as the ascii value of the
+     * index and the frequency the data of the index.
+     *
+     * @param count array with integers that correspond to the frequency of the index's ascii character
+     */
     public HuffmanTree(int[] count){
         Queue<HuffmanNode> treeBuilder = new PriorityQueue<>();
+        //Populate priority queue with all valid nodes
         for (int i = 0; i < count.length; i++) {
             if(count[i] > 0){
                 treeBuilder.add(new HuffmanNode((char) i, count[i]));
@@ -17,6 +35,7 @@ public class HuffmanTree {
         //Add Pseudo EOF character
         treeBuilder.add((new HuffmanNode((char) count.length, 1)));
 
+        //Build the Huffman tree
         while(treeBuilder.size() > 1){
             HuffmanNode left = treeBuilder.remove();
             HuffmanNode right = treeBuilder.remove();
@@ -27,6 +46,8 @@ public class HuffmanTree {
     }
 
     /**
+     * Builds the Huffman tree from a .code file in standard (Preorder) form. Frequency is set to -1 since frequency is
+     * not transmitted over a .code file.
      *
      * @param input A Scanner linked to a file or stream of data that represents the code to build the tree from.
      *              Stream must not be empty.
@@ -34,12 +55,20 @@ public class HuffmanTree {
     public HuffmanTree(Scanner input){
         this.root = new HuffmanNode();
         while(input.hasNextLine()){
-            char character = (char) Integer.parseInt(input.nextLine());;
+            char character = (char) Integer.parseInt(input.nextLine());
             String address = input.nextLine();
             add(new HuffmanNode(character, -1), address, this.root);
         }
     }
 
+    /**
+     * Helper that adds a node to the huffman tree given the binary address. Used when creating the tree from .code file.
+     *
+     * @param node The node to add to the tree
+     * @param address The binary address the node needs to be added to. 0 = left, 1 = right
+     * @param current The node the recursive function is currently in.
+     * @return {@link HuffmanNode} Needed to use x = change(x) algorithm
+     */
     private HuffmanNode add(HuffmanNode node, String address, HuffmanNode current){
         String bit = address.substring(0,1);
         if(address.length() > 1){
@@ -68,46 +97,28 @@ public class HuffmanTree {
         return null;
     }
 
-    public void printPreorder(){
-        printPreorder(this.root);
-    }
-
-    private void printPreorder(HuffmanNode current){
-        if(current != null){
-            System.out.println(current);
-            printPreorder(current.getLeft());
-            printPreorder(current.getRight());
-        }
-    }
-
-    public void printPreorderFile(String file) throws FileNotFoundException {
-        printPreorderFile(this.root, new PrintStream(new File(file)));
-    }
-
-    private void printPreorderFile(HuffmanNode current, PrintStream file){
-        if(current != null){
-            file.println(current);
-            printPreorderFile(current.getLeft(), file);
-            printPreorderFile(current.getRight(), file);
-        }
-    }
-
-    public void printInorder(){
-        printInorder(this.root);
-    }
-
-    private void printInorder(HuffmanNode current){
-        if(current != null){
-            printPreorder(current.getLeft());
-            System.out.println(current);
-            printPreorder(current.getRight());
-        }
-    }
-
+    /**
+     * This method is used to write the Huffman Tree to a .code file in standard order (preorder).
+     * For each item stored in the tree takes up two lines of the file. The first one will be the ascii value
+     * of the character. The second one will be the binary "address" of that character in the tree. A 0 in the address
+     * represents a left. A 1 in the address represents a right. On re-creation of a {@link HuffmanTree} this address
+     * will be used for placement of the node within the tree.
+     *
+     * @param output The stream to output the Huffman tree to.
+     */
     public void write(PrintStream output) {
         this.write(output, this.root, "");
     }
 
+    /**
+     * A helper method that does the work of writing the tree to the .code file.
+     *
+     * @param output A stream to the output destination
+     * @param current The node the recursive write function is currently "in". Pass in a node and this method will
+     *                print everything below it to the stream.
+     * @param bitSequence The sequence of bits that represents the address within the tree. See above for
+     *                    meaning of address.
+     */
     private void write(PrintStream output, HuffmanNode current, String bitSequence) {
         if(current.isLeaf()){
             output.println((int) current.getCharacter());
@@ -118,6 +129,14 @@ public class HuffmanTree {
         }
     }
 
+    /**
+     * Decodes a .short file based on the stored Huffman tree.
+     *
+     * @param input An {@link BitInputStream} that will give the bits for the given .short file.
+     * @param output The stream to the .new file that will contain the decompressed file.
+     * @param charMax The pseudo-EOF character. This character represents the end of the compressed file.
+     *                It is used to know when to stop reading bits from the {@link BitInputStream}.
+     */
     public void decode(BitInputStream input, PrintStream output, int charMax) {
         HuffmanNode current = this.root;
         boolean reading = true;
@@ -126,7 +145,7 @@ public class HuffmanTree {
                 if(current.getCharacter() == (char) charMax){
                     reading = false;
                 } else {
-                    output.print(current.getCharacter());
+                    output.write(current.getCharacter());
                     current = this.root;
                 }
             } else {
